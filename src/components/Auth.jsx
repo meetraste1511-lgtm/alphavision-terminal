7db97f0ac1f07815656ded5289c1aa911583cdb0
Meet Raste <meetraste1511@gmail.com>
2026-04-28 20:04:59 +0530

    Fix JSX syntax error in Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -71,9 +71,6 @@ export default function Auth({ onLogin }) {
             </div>
           </div>
 
-            </div>
-          </div>
- 
           {!isForgotPassword && (
             <div className="input-group">
               <label>Password</label>
